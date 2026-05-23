3b42822ed87f86a43b16c9233cd6180a1b39adb3
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-05-23 15:08:33 +0700

    add show detail departments

---
+++ resources/views/departments/show.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.dashboard')
+@section('section')
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Detail Department</h3>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('departments.index') }}">Departments</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Detail Department</li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="col-md-12">
+                @if (session('error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert"><i
+                            class="bi bi-check-circle"></i>
+                        {{ session('error') }}.
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+    <section id="basic-horizontal-layouts">
+        <div class="row match-height">
+            <div class="col-md-12 col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <form class="form form-horizontal">
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <label for="first-name-horizontal">Name</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            <h6>: {{ $department->name }}</h6>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="email-horizontal">Description</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            <h6 class="text-justify">: {{ $department->description }}</h6>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="password-horizontal">Status</label>
+                                        </div>
+                                        <div class="col-md-8 form-group">
+                                            @if ($department->status === 'active')
+                                                <h6>: <span class="badge bg-success">Active</span></h6>
+                                            @elseif ($department->status === 'inactive')
+                                                <h6>: <span class="badge bg-secondary">Inactive</span></h6>
+                                            @endif
+                                        </div>
+                                        <div class="col-sm-12 d-flex mt-2">
+                                            <a href="{{ route('departments.index') }}" class="btn btn-secondary">Back to List</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection;
