a998b2d750c0f19bc8efd4853f8b82816479ec96
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-05-28 15:41:16 +0700

    fix layouting with hide breadcrumb on dashboard page

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -180,7 +180,7 @@
                         </div>
                         <div class="col-12 col-md-6 order-md-2 order-first">
                             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                                @if (!request()->is('dashboard') || !request()->is('/'))
+                                @if (!request()->is('dashboard') && !request()->is('/'))
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="@yield('link')">@yield('previous-title')</a>
                                     </li>
