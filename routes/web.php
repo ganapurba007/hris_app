cbc0a1defa39d6fb076d517ddc45c5e9a4550e03
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-07-31 09:54:04 +0700

    fix:delete ProfileController

--- routes/web.php
+++ routes/web.php
@@ -41,9 +41,9 @@ Route::middleware('auth')->group(function () {
     Route::resource('/leave_requests', LeaveRequestController::class)->middleware(['role:HR,Backend Developer,Frontend Developer,Finance Staff']);
 
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
